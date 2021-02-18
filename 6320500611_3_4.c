f24832bf944ac5576f77a0ef8a83425f177373a4
ChargeBrX <panuwat.janj@ku.th>
2021-02-18 17:50:35 +0700

    prime friend

--- 6320500611_3_4.c
+++ 6320500611_3_4.c
@@ -1,23 +1,49 @@
 #include <stdio.h>
-int main()
+long long int pf(long long int x)
 {
-    long long int x,p,i,j,a,b=2;
+    long long int i,j,a;
 
-    scanf("%lld",&x);
-
-    for(i=1;i<=9;i++)
-    {
-        b = b*10;
-    }
-
-    long long int n[x];
-
-    for(j=1; j<8; j++)
+    for(j=2; j<=x; j++)
     {
         if(x%j!=0)
         {
-            a = j;
+            if(j<x)
+            {
+                a = i;
+            }
+            else
+            {
+                a = x;
+            }
+
         }
+    }
+
+    return a;
+
+
+}
+int main()
+{
+    long long int x,p,i=0,j,b=2,a;
+
+    scanf("%lld",&x);
+
+    long long int n[x];
+
+    if(x==2)
+    {
+        a = 2;
+        printf("%d",n[0]);
+    }
+    else if(x==3)
+    {
+        a = 3;
+        printf("%d",n[0]);
+    }
+    else
+    {
+        a = pf(x);
     }
 
     printf("%d",a);
