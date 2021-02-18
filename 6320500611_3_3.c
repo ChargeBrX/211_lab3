c4de2ba13ae1a3769e34865b2d4c2c6ae3cd6958
ChargeBrX <panuwat.janj@ku.th>
2021-02-18 16:10:25 +0700

    Factorization

--- 6320500611_3_3.c
+++ 6320500611_3_3.c
@@ -1,34 +1,28 @@
 #include <stdio.h>
 int main()
 {
-    int k,i=2,n=0,b=0;
-
+    int k,i=2,n=1,b,x;
     scanf("%d",&k);
-
-    int a[k];
+    x=k;
    while(k>=i)
    {
        if(k%i==0)
        {
-           a[b] = i;
-           b++;
-           k = k/i;
-           n++;
+           b = i;
+           k = k/b;
+           n = n*b;
+           printf("%d\n",b);
+           if(n==x)
+           {
+               b=0;
+               printf("%d",b);
+           }
        }
        else
        {
            i++;
        }
    }
-   int x[n+1];
-   x[n] = 0;
-   a[n] = 0;
-   for(i=0;i<n+1;i++)
-   {
-       x[i]=a[i];
-       printf("%d\n",x[i]);
-   }
-
    return 0;
 
 }
